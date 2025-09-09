c0b36462a518b11092448d110fcd1924a3d8bf6c
bhanug99 <ganta.adithya99@gmail.com>
2025-09-09 20:06:06 +0530

    Prints a + b

--- 01_basics/first.py
+++ 01_basics/first.py
@@ -3,3 +3,9 @@
 
 print("Welcome to the World of Programming") # Prints Msg
 
+a = 10 # Assigns value of 10 to a
+b = 20 # Assigns value of 20 to b
+
+c = a+b # Adds a value of a and b to c 
+
+print(c) # Prints Value of C to Screen
