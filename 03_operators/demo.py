a7dc07e5a78ff7d19cec268d1ff2f6c1169d8f20
bhanug99 <ganta.adithya99@gmail.com>
2025-09-11 16:36:03 +0530

    # Arithmetic Operators

---
+++ 03_operators/demo.py
@@ -0,0 +1,14 @@
+# Operators
+
+# Arithmetic Operators
+num1 = 3
+num2 = 2
+print(f"Sum of {num1} and {num2} is {num1+num2}")
+print(f"Difference of {num1} and {num2} is {num1-num2}")
+print(f"Product of {num1} and {num2} is {num1*num2}")
+print(f"Division of {num1} and {num2} is {num1/num2}")
+print(f"Modulus of {num1} and {num2} is {num1%num2}")
+
+print(f"Floor Division of {num1} and {num2} is {num1//num2}")
+print(f"Result of {num1} power {num2} is {num1**num2}") # 3 ^ 2
+
