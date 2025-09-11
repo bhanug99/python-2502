de5867048a81ac37b4e3ed755a05bc40bab49e47
bhanug99 <ganta.adithya99@gmail.com>
2025-09-11 16:36:44 +0530

    Compound Assignment Operators

--- 03_operators/demo.py
+++ 03_operators/demo.py
@@ -12,3 +12,23 @@ print(f"Modulus of {num1} and {num2} is {num1%num2}")
 print(f"Floor Division of {num1} and {num2} is {num1//num2}")
 print(f"Result of {num1} power {num2} is {num1**num2}") # 3 ^ 2
 
+# Compound Assignment Operators
+num = 10
+num = num + 5 # without Compound Assignment Operators
+print(num)
+
+num = 10
+num+=5
+print(num) # with Compound Assignment Operators
+num*=5
+print(num)
+
+count = 1
+# increment count 
+count += 1
+print(count)
+
+count = 10
+# decrement count 
+count -= 1
+print(count)
