0799485842cab662a0054765e1244461c32e2708
bhanug99 <ganta.adithya99@gmail.com>
2025-09-12 17:14:45 +0530

    Complex Data Types

--- 04_data_types/demo.py
+++ 04_data_types/demo.py
@@ -21,3 +21,25 @@ print(type(data))
 # boolean 
 data = False
 print(type(data))
+
+# List (homogenous data)
+data = [10,20,30,40,50]
+data = [101,"Ravi",True] 
+print(type(data))
+
+# Tuple (heterogenous data)
+data = (101,"Ravi",True)
+data = (10,20,30,40,50)
+print(type(data))
+
+# Set 
+data = {10,20,30,40,50}
+print(type(data))
+
+# Dictionary
+data = {"name":"Ravi", "id":101, "location":"hyd"}
+print(type(data))
+
+# None Type
+x = None
+print(type(x))
