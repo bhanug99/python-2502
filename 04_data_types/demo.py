2a7ca2380932a7d1617dca2ac1bad35c017229e2
bhanug99 <ganta.adithya99@gmail.com>
2025-09-12 17:13:21 +0530

    Basic Data Types

---
+++ 04_data_types/demo.py
@@ -0,0 +1,23 @@
+# Data Types
+
+# Numeric Types 
+
+# int 
+data = 10
+print(type(data))
+
+# float
+data = 10.5
+print(type(data))
+
+# complex 
+data = 3 + 5j
+print(type(data))
+
+# string
+data = "python"
+print(type(data))
+
+# boolean 
+data = False
+print(type(data))
