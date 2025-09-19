95801bef0fc650a9d5298fff2d8b07673a440657
bhanug99 <ganta.adithya99@gmail.com>
2025-09-19 22:43:57 +0530

    Strings

---
+++ demoo.py
@@ -0,0 +1,19 @@
+#                0  1  2  3  4  5
+#sample_text = " p  y  t  h  o  n"
+#               -6 -5 -4 -3 -2 -1
+sample_text = "python"
+print(sample_text[:])
+print(sample_text[0:6])
+print(sample_text[1:6])
+print(sample_text[2:6])
+print(sample_text[3:6])
+print(sample_text[4:6])
+print("="*30)
+#print(sample_text[-6:0])
+print(sample_text[-6:-1])
+print(sample_text[-5:-1])
+print(sample_text[-5:-2])
+print(sample_text[-5:-3])
+print(sample_text[-5:-4])
+print("="*30)
+print(sample_text[6:0:-1])
