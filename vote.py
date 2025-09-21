4105d82438e6464bdcd7c9cd4a8060a64da923ab
bhanug99 <ganta.adithya99@gmail.com>
2025-09-21 22:01:28 +0530

    Vote Base

---
+++ vote.py
@@ -0,0 +1,6 @@
+name=input("Enter Your Name : ")
+age=int(input("Enter Your Age : "))
+mobile_num = input("Enter Your Phone Number : ")
+#id_generated = name[:2].upper mobile_num[-4:0]
+print(f"Hello {name}, Welcome to Voting Application")
+
