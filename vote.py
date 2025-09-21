06802dbc11df9c53bfa49183efc3b3fc936ac6cb
bhanug99 <ganta.adithya99@gmail.com>
2025-09-21 22:12:25 +0530

    Age Added

--- vote.py
+++ vote.py
@@ -3,4 +3,7 @@ age=int(input("Enter Your Age : "))
 mobile_num = input("Enter Your Phone Number : ")
 #id_generated = name[:2].upper mobile_num[-4:0]
 print(f"Hello {name}, Welcome to Voting Application")
-
+if age <= 18 :
+    print("Age is OK")
+else:
+    print("Not Accepted")
